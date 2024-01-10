32cd0fe3d60afc9d88cb5aa4e9a5f0bc1ec15721
AbuKhairAnis <abukhairanis123@gmail.com>
2024-01-11 00:47:55 +0600

    create project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import "./App.css";z
+
+function App() {
+  return (
+    <>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  );
+}
+
+export default App;
