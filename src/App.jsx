68f0489c0c86f1098094a607593d55f73ba1204d
rashadrabby <rashadulislamrabby@gmail.com>
2024-01-12 00:31:58 +0600

    'Tailwind_install_with_DaisyUI'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-// import "./App.css";z
+import "./App.css";
 
 function App() {
   return (
     <>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
+
+      <h1>Welcome To SoftyCraft</h1>
+      <button
+        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
     </>
   );
 }
