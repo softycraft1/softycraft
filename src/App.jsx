e724b3a49397123d8c354f80e9fbe046903cde4a
rashadrabby <rashadulislamrabby@gmail.com>
2024-01-12 00:57:02 +0600

    'navbar_added'

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import "./App.css";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <>
-
-      <h1>Welcome To SoftyCraft</h1>
-      <button
-        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-        Button
-      </button>
+      <Navbar />
+      <h1 className="text-5xl">Welcome To <span className="text-green-800">SoftyCraft</span></h1>
     </>
   );
 }
